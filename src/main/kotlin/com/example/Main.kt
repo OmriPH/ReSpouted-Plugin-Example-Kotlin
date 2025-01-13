3216c183fbb1339140c8e89608b807aca74a7ee3
Omri H <omri.hermon@gmail.com>
2025-01-13 15:22:47 +0200

    recipes

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,6 +1,13 @@
 package com.example
 
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.getspout.spoutapi.SpoutManager
+import org.getspout.spoutapi.inventory.SpoutItemStack
+import org.getspout.spoutapi.inventory.SpoutShapedRecipe
+import org.getspout.spoutapi.inventory.SpoutShapelessRecipe
+import org.getspout.spoutapi.material.MaterialData
 
 class Main : JavaPlugin() {
     lateinit var exampleItem: ExampleItem
@@ -13,6 +20,18 @@ class Main : JavaPlugin() {
         exampleItem = ExampleItem()
         exampleBlock = ExampleBlock()
         exampleFood = ExampleFood()
+
+        // Basic 2x2 shaped recipe
+        SpoutManager.getMaterialManager().registerSpoutRecipe(
+            SpoutShapedRecipe(SpoutItemStack(exampleBlock, 1))
+                .shape("ss", "ss")
+                .setIngredient('s', MaterialData.sugarCane)
+        )
+        // Shapeless recipe
+        SpoutManager.getMaterialManager().registerSpoutRecipe(
+            SpoutShapelessRecipe(ItemStack(Material.SUGAR_CANE, 4))
+                .addIngredient(1, exampleBlock)
+        )
     }
 
     companion object {
